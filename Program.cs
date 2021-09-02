5b996e4f3ceadc45ed0c2e842d8b8049da075d2a
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2021-09-02 12:15:14 +0430

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -26,31 +26,51 @@ namespace ConsoleApp5
 
         static void Main(string[] args)
         {
+            // watch to determine execution time
             var watch = new Stopwatch();
             watch.Start();
 
+            // reading the file
             ImageFileReader imageFileReader = new ImageFileReader();
+            /*
+             your directory including mhd and raw file should be set here
+             */
             imageFileReader.SetFileName("C:\\Users\\m3xpgag\\source\\repos\\" +
                 "ConsoleApp5\\ConsoleApp5\\sample.mhd");
             imageFileReader.SetOutputPixelType(PixelIDValueEnum.sitkFloat32);
             imageFileReader.SetImageIO("MetaImageIO");
             Image image=imageFileReader.Execute();
-
-
             ImageFileWriter imageFileWriter = new ImageFileWriter();
             imageFileWriter.KeepOriginalImageUIDOn();
-           
 
-          //  \\|//
-          //  //|\\
+         /*   # Write the 3D image as a series
+              # IMPORTANT: There are many DICOM tags that need to be updated when you modify
+              # an original image. This is a delicate opration and requires
+              # knowledge of the DICOM standard. This example only modifies some.
+              # For a more complete list of tags that need to be modified see:
+              # http://gdcm.sourceforge.net/wiki/index.php/Writing_DICOM
+              # If it is critical for your work to generate valid DICOM files,
+              # It is recommended to use David Clunie's Dicom3tools to validate
+              # the files:
+              # http://www.dclunie.com/dicom3tools.html
+            */
 
-            String modification_time= Getcurrentime();
+            //  \\|//
+            //  //|\\
+
+            String modification_time = Getcurrentime();
             String modification_date= Getcurrendate();
 
             VectorDouble direction_Vec = image.GetDirection();
             double[] direction = direction_Vec.ToArray();
-            //Console.WriteLine(conc(direction));
 
+            /*
+             these tags should be wrote
+             # Copy some of the tags and add the relevant tags indicating the change.
+             # For the series instance UID (0020|000e), each of the components is a number,
+             # cannot start with zero, and separated by a '.' We create a unique series ID
+             # using the date and time. Tags of interest:
+               */
             String rescale_slope = "0.001";
 
             var series_tag_values = new ArrayList()
@@ -71,21 +91,31 @@ namespace ConsoleApp5
                 new tup_string("0028|0103", "1") 
                 };
 
+            /*
+             write all 2D slice of mhd image
+             */
             for(uint i = 0; i < image.GetDepth(); i++)
             {
                 Write_slice(series_tag_values, image, i, imageFileWriter);
-               // Console.WriteLine(i);
             }
             watch.Stop();
 
-            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Final Execution Time: {watch.ElapsedMilliseconds} ms");
             Console.ReadKey();
         }
 
         static void Write_slice(ArrayList series_tag_values, Image im3d,uint depth, ImageFileWriter ifw)
         {
-            
+            /*
+            write one of the 3d image slices with given depth
+             */
+            var watch = new Stopwatch();
+            watch.Start();
             Image im2d = getnth2dsliceof3dimage(im3d, depth);
+
+            watch.Stop();
+
+            Console.WriteLine($"getnth2dsliceof3dimage Execution Time: {watch.ElapsedMilliseconds} ms");
             foreach (tup_string tup in series_tag_values.ToArray()) {
                 im2d.SetMetaData(tup.x, tup.y);
             }
@@ -104,13 +134,14 @@ namespace ConsoleApp5
            
             
             string st ="\\"+ conc(drr.ToArray());
-
             im2d.SetMetaData("0020|0032", st);
-
             im2d.SetMetaData("0020,0013", depth.ToString());
 
+            /*
+             your directory Saving Dicoms should be set here
+             */
             string basePath = @"C:\Users\m3xpgag\source\repos\ConsoleApp5\ConsoleApp5\di";
-
+            //the Dicom Suffix
             string filePath = ".dcm";
             string combinedPath = Path.Combine(basePath, (depth.ToString() + filePath));
            
@@ -120,6 +151,10 @@ namespace ConsoleApp5
 
         static string conc(double[] cot)
         {
+            /*
+             simple method to Convert double to str
+             then concat them
+             */
             StringBuilder sb = new StringBuilder();
             for(int i=0; i<cot.Length; i++) {
                 if (i != 0)
@@ -132,11 +167,14 @@ namespace ConsoleApp5
         }
 
         static Image getnth2dsliceof3dimage(Image im3d,uint n) {
-            //uint[] arr = im3d.GetSize().ToArray();
+            /**
+             * take one of slice of 3d image with given depth
+             * this method needs Complexity improvement
+             * I get the 2d-image by Every pixel of given depth
+             * then inserting all in a new 2d image
+             * **/
             Image im2d = new Image(im3d.GetWidth(),im3d.GetHeight(),PixelIDValueEnum.sitkFloat32);
-            //Console.WriteLine(im2d.GetDimension());
-            //\\im2d.
-           // VectorUInt32 vui = new VectorUInt32(new uint[] { 511, 511, 69 });
+         
 
             for (uint i = 0; i <  im3d.GetWidth(); i++)
             {
